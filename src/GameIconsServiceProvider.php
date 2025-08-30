3bf9fbddbf994849f6dd10b8a81973f1cb0f3750
alizharb <harbzali@gmail.com>
2025-08-30 19:59:30 -0400

    Added Content directory for filamentphp plugin

--- src/GameIconsServiceProvider.php
+++ src/GameIconsServiceProvider.php
@@ -5,6 +5,8 @@ namespace Alizharb\FilamentGameIcons;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Alizharb\FilamentGameIcons\Commands\SyncGameIconsCommand;
+use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Assets\Css;
 
 class GameIconsServiceProvider extends PackageServiceProvider
 {
@@ -15,5 +17,20 @@ class GameIconsServiceProvider extends PackageServiceProvider
         $package
             ->name('filament-game-icons')
             ->hasCommand(SyncGameIconsCommand::class);
+    }
+
+    public function bootingPackage(): void
+    {
+        FilamentAsset::register([
+            Css::make('game-icons', __DIR__ . '/../resources/css/game-icons.css'),
+        ], self::$name);
+
+        $this->publishes([
+            __DIR__ . '/../resources/css/game-icons.css' => resource_path('vendor/filament-game-icons/game-icons.css'),
+        ], 'filament-game-icons-styles');
+
+        $this->publishes([
+            __DIR__ . '/../resources/css/game-icons.css' => public_path('vendor/filament-game-icons/game-icons.css'),
+        ], 'filament-game-icons-public');
     }
 }
